86b0044b1a69873c21435a742a8304193c50c418
Jakub741 <kuba.ki@seznam.cz>
2022-07-26 18:54:15 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import Table from 'react-bootstrap/Table';
+import { Container } from 'react-bootstrap';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import React, { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import studentData from "./mock-data.json";
+import subjectData from "./mock-data-subject.json";
+import groupData from "./mock-data-group.json";
+import dateData from"./mock-data-date.json";
+
+
+
+const App = () => {
+
+  const [students, setStudents] = useState(studentData);
+  const [subjects, setSubjects] = useState(subjectData);
+  const [groups, setGroups] = useState(groupData);
+  const[dates,setDates] = useState(dateData);
+
+  const subjectByID = subjects[2];
+  const groupByID = groups[0];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Container fluid>
+      <Row>
+        <Col>
+        <h1>
+            {subjectByID.name}
+        </h1>
+        <h2>
+          {groupByID.name}
+        </h2>
+          
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Table striped bordered hover>
+                <thead>
+                  <tr>
+                    <th>Jméno</th>
+                    <th>Příjmení</th>
+                    <th>Termín</th>
+                    <th>Známka</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {students.map((student),dates.map((date) => (
+                    <tr>
+                    <td>{student.name}</td>
+                    <td>{student.surname}</td>
+                    <td>{date.date}</td>
+                    <td>{student.grade}</td>
+                  </tr>
+                    )))}
+                  
+                </tbody>
+          </Table>
+        </Col>
+      </Row>
+      
+    </Container>
+  )
 }
 
-export default App;
+export default App
+
